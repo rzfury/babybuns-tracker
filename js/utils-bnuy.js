39c40011c782adcca3742e2263a4a387e535d42e
RZFury <48477044+rzfury@users.noreply.github.com>
2023-08-12 15:15:39 +0700

    add more bunny classification

--- js/utils-bnuy.js
+++ js/utils-bnuy.js
@@ -13,14 +13,28 @@ function getBurrowDepth(bun) {
 }
 
 function isTempleBunny(burrowArea, depth) {
+  depth = parseInt(depth)
+
   if (burrowArea === 'C' && depth === 13) return true;
-  // TODO: Add other specification for temple bnuy, I still dont know
+  if (burrowArea === 'W' && (depth >= 13 && depth <= 18)) return true;
+  if (burrowArea === 'N' && (depth >= 12 && depth <= 18)) return true;
+  if (burrowArea === 'S' && (depth >= 13 && depth <= 20)) return true;
+  if (burrowArea === 'E' && (depth === 13)) return true; // temporary
+  // TODO: Add other classification for temple bnuy, I still dont know
+
   return false;
 }
 
 function isHellBunny(burrowArea, depth) {
-  if (burrowArea === 'C' && depth >= 13) return true;
-  // TODO: Add other specification for hell bnuy, I still dont know
+  depth = parseInt(depth)
+
+  if (burrowArea === 'C' && (depth > 14 && depth <= 26)) return true;
+  if (burrowArea === 'W' && (depth >= 19 && depth <= 26)) return true;
+  if (burrowArea === 'N' && (depth >= 19 && depth <= 23)) return true;
+  if (burrowArea === 'S' && (depth >= 21 && depth <= 23)) return true;
+  if (burrowArea === 'E' && (depth >= 14 && depth <= 22)) return true;
+  // TODO: Add other classification for hell bnuy, I still dont know
+
   return false;
 }
 
